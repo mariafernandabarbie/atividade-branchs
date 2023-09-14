713d2328816fbdadac5a392718a4fe4408e436da
mariafernandabarbie <maria.souza27@estudante.ifms.edu.br>
2023-09-13 20:00:20 -0400

    deslocamento

--- js/js.js
+++ js/js.js
@@ -14,3 +14,27 @@ function Barreira(reversa = false){
 
     this.setAltura = altura => corpo.style.height = `${altura}px`
 }
+
+function ParDeBarreiras(altura, abertura, x){
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
