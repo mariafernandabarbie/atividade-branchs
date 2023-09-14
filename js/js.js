2a241274908867c4794312796431edf0fe5751f8
mariafernandabarbie <maria.souza27@estudante.ifms.edu.br>
2023-09-13 20:31:54 -0400

    recomecar

--- js/js.js
+++ js/js.js
@@ -125,3 +125,32 @@ function colidiu(passaro, barreiras){
     })
     return colidiu
 }
+
+function FlappyBird() {
+    let pontos = 0
+
+    const areaDoJogo = document.querySelector('[wm-flappy]')
+    const altura = areaDoJogo.clientHeight
+    const largura = areaDoJogo.clientWidth
+
+    const progresso = new Progresso()
+    const barreiras = new Barreiras(altura, largura, 200, 400,
+        () => progresso.atualizarPontos(++pontos))
+        const passaro = new Passaro(altura)
+
+        areaDoJogo.appendChild(progresso.elemento)
+        areaDoJogo.appendChild(passaro.elemento)
+        barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+        this.start = () => {
+            const temporizador = setInterval(() => {
+                barreiras.animar()
+                passaro.animar()
+
+                if(colidiu(passaro, barreiras)){
+                    clearInterval(temporizador)
+                }
+            }, 20)
+        }
+}
+
